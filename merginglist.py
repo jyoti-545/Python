6ec2124b0f193aea65ad86ad4bdb2af9a47b3703
geetanshukushwaha <66560106+geetanshukushwaha@users.noreply.github.com>
2020-07-29 19:33:07 +0530

    Add files via upload

---
+++ merginglist.py
@@ -0,0 +1,37 @@
+
+# merging two lists whose values are entered by user by creating a fuction
+
+
+n = int(input('Total numbers in first list :'))
+
+a = []
+
+for i in range(n):
+    x = int(input('Enter the number :'))
+    a.append(x)
+
+print('First list',a)
+
+m = int(input('Total numbers in second list :'))
+b = []
+
+for i in range(m):
+    x = int(input('Enter the number :'))
+    b.append(x)
+
+
+print('Second list',b)
+
+l = []
+def merge():
+    for i in a:
+        l.append(i)
+
+    for i in b:
+        l.append(i)
+    print('Merged List',l)
+
+merge()
+
+
+        
