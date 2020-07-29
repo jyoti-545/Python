346aff853065172958221cf243a7162f96cf7b81
geetanshukushwaha <66560106+geetanshukushwaha@users.noreply.github.com>
2020-07-29 19:22:00 +0530

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,26 @@
+
+
+n = int(input('Enter the number of elements you want in the list: '))
+
+l = []
+
+def list():
+    for i in range(n):
+        x = int(input('Enter the number: '))
+        l.append(x)
+    return l
+
+l = list()
+
+def multi():
+    a = 1
+    for i in l:
+        a *= i
+
+    return a
+
+
+a = multi()
+
+print('Multipication of all numbers of list =',a)
+
