5e627f6620ad50a7da247b859ed83d1e59171a6f
Geetanshu Kumar Kushwaha <66560106+geetanshukushwaha@users.noreply.github.com>
2023-08-19 19:07:22 +0530

    Update Stone_paper_scissor.py

--- Stone_paper_scissor.py
+++ Stone_paper_scissor.py
@@ -1,75 +1,42 @@
-
 import random
 
-print()
-print()
-print("----------------------HI----------------------")
-print("-------------WELCOME TO OUR GAME--------------")
-print()
-
+print("\n----------------------HI----------------------")
+print("-------------WELCOME TO OUR GAME--------------\n")
 print("              STONE PAPER SCISSOR             ")
 
-name = str(input("Please enter your name : "))
+name = str(input("\nPlease enter your name : "))
+print("\n---------------Hello",name+"----------------")
 
-print()
-
-print("---------------Hello",name+"----------------")
-
-
-print("-----------------Game Info------------------")
-print()
+print("\n-----------------Game Info------------------")
 print("You have to type what you choose stone, paper or scissor")
-print()
-print("Letter case doesn't matter")
-print()
-
-print("--------Are you ready for the game----------")
+print("Letter case doesn't matter\n")
+print("Are you ready for the game? ")
 input("press enter  to start   : ")
 
-
-print()
-
-
 l = ('stone','paper','scissor')
-
 while True:
-
     c = str(input("Enter what you choose  :   "))  
-
     c = c.lower()
-
     a = random.randrange(len(l))
-
     a = l[a]
-
     print(a)
 
     if a == c :
-        print('------------You Won------------')
-
+        print('-----------Match Draw-----------')
     elif a == 'stone' and c == 'paper' :
         print('------------You Won------------')
-
     elif a == 'paper' and c == 'scissor':
         print('------------You Won------------')
-
     elif a == 'scissor' and c == 'stone':
-        print('------------You Won-------------')
-        
+        print('------------You Won-------------')    
     else:
-        print('-----Better Luck Next Time-----')
+        print('-----Better Luck Next Time-----\n')
                 
-    print()
     x = input("Press y you want to continue and any other key if you want to exit  : ")
-
-    print()
-    print()
 
     if x == 'y':
         continue
-
     else:
         print("-----------Thank you for playing --------------")
         print("----------Hope your enjoyed the Game ----------")
-
         break
