924c2f81da6f460a271626d8bfedd8437770ceb0
Jyoti Kushwaha <115943246+jyoti-545@users.noreply.github.com>
2023-08-19 19:16:04 +0530

    Update Stone_paper_scissor.py

--- Stone_paper_scissor.py
+++ Stone_paper_scissor.py
@@ -1,23 +1,14 @@
-
 import random
 
-print()
-print()
 print("----------------------HI----------------------")
 print("-------------WELCOME TO OUR GAME--------------")
-print()
 
 print("              STONE PAPER SCISSOR             ")
-
 name = str(input("Please enter your name : "))
-
-print()
-
 print("---------------Hello",name+"----------------")
 
 
 print("-----------------Game Info------------------")
-print()
 print("You have to type what you choose stone, paper or scissor")
 print()
 print("Letter case doesn't matter")
@@ -26,50 +17,28 @@ print()
 print("--------Are you ready for the game----------")
 input("press enter  to start   : ")
 
-
-print()
-
-
 l = ('stone', 'paper', 'scissor')
-
 while True:
-
     c = str(input("Enter what you choose  :   "))  
-
     c = c.lower()
-
     a = random.randrange(len(l))
-
     a = l[a]
-
     print(a)
-
     if a == c :
-        print('------------You Won------------')
-
+        print('-----------Match Draw-----------')
     elif a == 'stone' and c == 'paper' :
         print('------------You Won------------')
-
     elif a == 'paper' and c == 'scissor':
         print('------------You Won------------')
-
     elif a == 'scissor' and c == 'stone':
-        print('------------You Won-------------')
-        
+        print('------------You Won-------------')        
     else:
-        print('-----Better Luck Next Time-----')
-        
-    print()
+        print('-----Better Luck Next Time-----\n')        
     x = input("Press y you want to continue and any other key if you want to exit  : ")
-
-    print()
-    print()
 
     if x == 'y':
         continue
-
     else:
         print("-----------Thank you for playing --------------")
         print("----------Hope your enjoyed the Game ----------")
-
         break
