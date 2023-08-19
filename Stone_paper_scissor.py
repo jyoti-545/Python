55bae72e43e20c047ae268ed4088e6350e0293c8
Jyoti Kushwaha <115943246+jyoti-545@users.noreply.github.com>
2023-08-19 18:42:20 +0530

    Update Stone_paper_scissor.py

--- Stone_paper_scissor.py
+++ Stone_paper_scissor.py
@@ -30,7 +30,7 @@ input("press enter  to start   : ")
 print()
 
 
-l = ('stone','paper','scissor')
+l = ('stone',' paper', 'scissor')
 
 while True:
 
@@ -46,10 +46,19 @@ while True:
 
     if a == c :
         print('------------You Won------------')
+
+    elif a == 'stone' and c == 'paper' :
+        print('------------You Won------------')
+
+    elif a == 'paper' and c == 'scissor':
+        print('------------You Won------------')
+
+    elif a == 'scissor' and c == 'stone':
+        print('------------You Won-------------')
         
     else:
         print('-----Better Luck Next Time-----')
-        
+    
     print()
     x = input("Press y you want to continue and any other key if you want to exit  : ")
 
