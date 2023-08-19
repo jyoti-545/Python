5f5fa5933cfd9ff696853c192ed5d309dfa8c824
Geetanshu Kumar Kushwaha <66560106+geetanshukushwaha@users.noreply.github.com>
2023-08-19 19:18:15 +0530

    Update Stone_paper_scissor.py

--- Stone_paper_scissor.py
+++ Stone_paper_scissor.py
@@ -1,29 +1,26 @@
 import random
 
-print("----------------------HI----------------------")
-print("-------------WELCOME TO OUR GAME--------------")
-
+print("\n----------------------HI----------------------")
+print("-------------WELCOME TO OUR GAME--------------\n")
 print("              STONE PAPER SCISSOR             ")
-name = str(input("Please enter your name : "))
-print("---------------Hello",name+"----------------")
 
+name = str(input("\nPlease enter your name : "))
+print("\n---------------Hello",name+"----------------")
 
-print("-----------------Game Info------------------")
+print("\n-----------------Game Info------------------")
 print("You have to type what you choose stone, paper or scissor")
-print()
-print("Letter case doesn't matter")
-print()
-
-print("--------Are you ready for the game----------")
+print("Letter case doesn't matter\n")
+print("Are you ready for the game? ")
 input("press enter  to start   : ")
 
-l = ('stone', 'paper', 'scissor')
+l = ('stone','paper','scissor')
 while True:
     c = str(input("Enter what you choose  :   "))  
     c = c.lower()
     a = random.randrange(len(l))
     a = l[a]
     print(a)
+
     if a == c :
         print('-----------Match Draw-----------')
     elif a == 'stone' and c == 'paper' :
@@ -31,9 +28,10 @@ while True:
     elif a == 'paper' and c == 'scissor':
         print('------------You Won------------')
     elif a == 'scissor' and c == 'stone':
-        print('------------You Won-------------')        
+        print('------------You Won-------------')    
     else:
-        print('-----Better Luck Next Time-----\n')        
+        print('-----Better Luck Next Time-----\n')
+                
     x = input("Press y you want to continue and any other key if you want to exit  : ")
 
     if x == 'y':
